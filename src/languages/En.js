e5b36d814d9f1c2cbc390c6335538578be372ff3
rashidbik <abdurashid.bigzad1999@gmail.com>
2023-02-07 09:41:03 +0430

    jwt problems fixed

--- src/languages/En.js
+++ src/languages/En.js
@@ -11,7 +11,7 @@ export const En = {
     groups: ['Income', 'Expens'],
     header: ['Search', 'Setting'],
     footer: ['Group','Report','Home'],
-    setting: ['Setting', 'ٔNew Account', 'Login', 'Account', 'Profile', 'Language', 'About', 'Theme'],
+    setting: ['Setting', 'ٔNew Account', 'Logout', 'Account', 'Profile', 'Language', 'About', 'Theme'],
     register: ['SIGN UP','Insert Your Name', 'What Is Your Job', 'exapmle@mail.com', 'Insert A Password', 'Register'],
     login: ['SIGN IN','Enter Your Email', 'Enter Your Password'],
     theme: ['Select Theme'],
--- src/setting/Settings.jsx
+++ src/setting/Settings.jsx
@@ -4,8 +4,12 @@ import authContext from '../helper/AuthContext'
 
 const Settings = () => {
   const navigate = useNavigate()
-  const {lang, auth, color } = useContext(authContext);
-   
+  const {lang, auth, setAuth, color } = useContext(authContext);
+   const logout = () => {
+    setAuth(false)
+    localStorage.removeItem('accessToken');
+    navigate('/');
+   }
   return (
     <div className='grid md:h-full '>
       <div style={color.C1} className='p-4 text-center md:hidden'>{lang.setting[0]}</div>
@@ -17,7 +21,7 @@ const Settings = () => {
         </span>
         <p className='px-4'>{lang.setting[1]}</p>
       </div>
-      <div style={color.C1} onClick={()=> navigate('/login')} className="p-4 flex">
+      <div style={color.C1} onClick={()=> logout()} className="p-4 flex">
         <span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
